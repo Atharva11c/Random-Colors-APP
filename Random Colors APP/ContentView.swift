6065cd492410769263ea434fe806941c18af902a
Atharva11c <lavhateatharva60@gmail.com>
2025-08-11 19:45:55 +0530

    Initial Commit

---
+++ Random Colors APP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Random Colors APP
+//
+//  Created by Atharva Lavhate on 11/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Random Colors APP/Random_Colors_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Random_Colors_APPApp.swift
+//  Random Colors APP
+//
+//  Created by Atharva Lavhate on 11/08/25.
+//
+
+import SwiftUI
+
+@main
+struct Random_Colors_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
